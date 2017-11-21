42c0f4ae8665651c4035b9a3c001a71595a540dd
Ildar Sakaev <ildar_cb@mail.ru>
2017-11-21 10:33:02 +0500

    Добавлены функции поиска максимума и отрисовки текста

--- js/stat.js
+++ js/stat.js
@@ -13,28 +13,35 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-  var max = -1;
-
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
+  var maximum = function () {
+    var max = -1;
+    for (var i = 0; i < times.length; i++) {
+      var time = times[i];
+      if (time > max) {
+        max = time;
+      }
     }
+    return max;
   }
-
   var histogramheight = 150;
-  var step = histogramheight / max;
+  var step = histogramheight / maximum ();
   var barWidth = 40;
   var indent = 50;
   var initialX = 140;
   var initialY = 240;
   var lineHeight = 18;
+  var playerScore = function (count) {
+    return ctx.fillText(names[count], initialX + (barWidth + indent) * count, initialY + lineHeight);
+  };
+  var playerName = function (count) {
+    return ctx.fillText(Math.round(times[count]), initialX + (barWidth + indent) * count, initialY - times[count] * step - lineHeight / 2);
+  };
 
-  for (i = 0; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     ctx.fillStyle = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + Math.random() + ')';
     ctx.fillRect(initialX + (barWidth + indent) * i, initialY - times[i] * step, barWidth, times[i] * step);
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(names[i], initialX + (barWidth + indent) * i, initialY + lineHeight);
-    ctx.fillText(Math.round(times[i]), initialX + (barWidth + indent) * i, initialY - times[i] * step - lineHeight / 2);
+    playerScore (i);
+    playerName (i);
   }
 };
